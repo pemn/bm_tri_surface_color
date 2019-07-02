2bc65883d460173dc3676a043edf0894a37a7d20
Paulo Ernesto <pemn@users.noreply.github.com>
2019-07-02 08:33:30 -0300

    Add files via upload

---
+++ voxelschema.py
@@ -0,0 +1,96 @@
+#!python
+# Copyright 2019 Vale
+# voxelscheme
+# v1.0 05/2019 paulo.ernesto
+
+import numpy as np
+import csv
+
+
+# normalized voxel cube around all xyz points of a csv file
+class Voxelschema(np.ma.MaskedArray):
+  def __new__(cls, size, dimensions):
+    if len(size) > 3:
+      size = size[0:3]
+    self = super().__new__(cls, np.ndarray(dimensions), np.ones(np.prod(dimensions), np.bool_))
+    self._bl = np.array(size)
+    self._o0 = np.zeros(3);
+    return(self)
+
+  # return the ijk of a voxel using our fixed voxel size instead of the voxel own size
+  def ijk(self, xyz):
+    #(block_centre, block_length) = Voxel.block(self._data.ix[c])
+    #return tuple(int((xyz[i] - self._o0[i]) // self._bl[i]) for i in range(3))
+    return np.divide(np.subtract(xyz, self._o0), self._bl).astype(np.int_)
+
+  def xyz(self, ijk):
+    return np.add(np.multiply(ijk, self._bl), self._o0)
+
+  def block_extent(self, xyzc, xyzl):
+    xyzr = np.multiply(xyzl, 0.5)
+    return np.subtract(xyzc, xyzr), np.add(xyzc, xyzr)
+
+  def flag_by_block(self, xyzc, xyzl, value, mode = 'first'):
+    xyz0, xyz1 = self.block_extent(xyzc, xyzl)
+    return self.flag_by_extent(xyz0, xyz1, value, mode)
+
+  # flag grid cells that are touched by a block
+  # in the case of multiple blocks touching a cell
+  # use the block which intersects with the greater volume
+  # input: block corners
+  def flag_by_extent(self, xyz0, xyz1, value, mode = 'first'):
+    ijk0 = self.ijk(xyz0)
+    ijk1 = self.ijk(xyz1)
+    # ijkd = np.subtract(ijk1, ijk0, dtype=np.int_, casting='unsafe')
+    ijkd = np.subtract(ijk1, ijk0)
+    for i in range(ijk0[0], ijk0[0] + ijkd[0]):
+      for j in range(ijk0[1], ijk0[1] + ijkd[1]):
+        for k in range(ijk0[2], ijk0[2] + ijkd[2]):
+          if self[i,j,k] is np.ma.masked:
+            pass
+          elif mode == 'max' and self[i,j,k] < value:
+            pass
+          elif mode == 'min' and self[i,j,k] > value:
+            pass
+          else:
+            continue
+
+          self[i,j,k] = value
+
+    return self
+
+  def get_k_minmax(self, i, j, mode = 'max'):
+    k = 0
+    ks = self[i, j, :]
+    if mode == 'max':
+      k = ks.argmax()
+    else:
+      k = ks.argmin()
+    return k
+
+  def get_2d_minmax(self, mode = 'max'):
+    grid = np.ma.MaskedArray(np.ndarray(self.shape[0:2]), np.ones(np.prod(self.shape[0:2]), np.bool_))
+    for i in range(self.shape[0]):
+      for j in range(self.shape[1]):
+        k = self.get_k_minmax(i, j, mode)
+        grid[i,j] = k
+
+    return grid
+
+  def save_2d_grid(self, path, grid):
+
+    with open(path, 'w', newline='') as csvfile:
+      csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+      csvwriter.writerow(['i', 'j','value'])
+      for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+          csvwriter.writerow([i, j, grid[i,j]])
+
+  def save_grid(self, path):
+    with open(path, 'w', newline='') as csvfile:
+      csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+      csvwriter.writerow(['i', 'j', 'k','value'])
+      for i in range(self.shape[0]):
+        for j in range(self.shape[1]):
+          for k in range(self.shape[2]):
+            csvwriter.writerow([i, j, k, self[i,j,k]])
---
+++ vulcan_mapfile.py
@@ -0,0 +1,89 @@
+#!python
+# Copyright 2019 Vale
+# parse vulcan map files using a DSL engine
+# v1.0 04/2018 paulo.ernesto
+
+import sys, os.path
+sys.path.insert(0, os.path.splitext(sys.argv[0])[0] + '.pyz')
+
+import lark
+
+#%import common.WS
+#%import common.CNAME
+mapfile_grammar = r"""
+  WS: /[ \t\f\r\n]/+
+  LCASE_LETTER: "a".."z"
+  UCASE_LETTER: "A".."Z"
+  LETTER: UCASE_LETTER | LCASE_LETTER
+  WORD: LETTER+
+  DIGIT: "0".."9"
+  CNAME: ("_"|LETTER) ("_"|LETTER|DIGIT)*
+
+  ?start: file
+
+  ?value: definition
+        | tab
+        | pair
+        | terminal
+
+  file: value+ "END" "$FILE"
+  COMMENT: /\*.*/
+  definition: "BEGIN" "$DEF" CNAME value+ "END" "$DEF" CNAME
+  tab: "BEGIN" "$TAB" CNAME value+ "END" "$TAB" CNAME
+  ESCAPED_STRING: "'" ("\\'"|/[^']/)* "'"
+  terminal: ESCAPED_STRING | CNAME
+  pair: CNAME "=" [terminal]
+
+
+  %ignore WS
+  %ignore COMMENT
+"""
+
+class MapfileTransformer(lark.Transformer):
+  pair = lambda self, _: [_[0].value, _[1]]
+  definition = lambda self, _: [_[0].value, _[1:-1]]
+  tab = lambda self, _: [_[0].value, _[1:-1]]
+  file = dict
+  terminal = lambda self, _: str.strip(_[0],"'")
+
+def mapfile_parse(input_path):
+  parser = lark.Lark(mapfile_grammar, parser='lalr', transformer=MapfileTransformer())
+  with open(input_path) as f:
+    return(parser.parse(f.read()))
+  return({})
+
+class VulcanScd(object):
+
+  def __init__(self, input_path, v_def = None, v_tab = None):
+    self._parse = mapfile_parse(input_path)
+    
+    self._device_color = None
+    if 'DEVICE_COLOUR' in self._parse:
+      self._device_color = self._parse['DEVICE_COLOUR'][0][1]
+
+    self._def = None
+    self._tab = None
+    if v_def is not None:
+      if v_def in self._parse:
+        self._def = self._parse[v_def]
+      
+      if v_tab is not None:
+        v_tab = str(v_tab).upper()
+        self._tab = dict(self._def)
+        if v_tab in self._tab:
+          self._tab = self._tab[v_tab]
+
+  def __getitem__(self, key):
+    if self._def is None or self._tab is None:
+      return(None)
+    c = None
+    for i in range(len(self._tab)):
+      if self._tab[i].upper() == str(key).upper():
+        c = int(self._tab[i - 1])
+        break
+    rgb = (1,1,1)
+    if c is not None and self._device_color is not None:
+      for i in range(0,len(self._device_color),4):
+        if int(self._device_color[i]) == c:
+          rgb = [float(self._device_color[_]) / 15.0 for _ in (i+1, i+3, i+2)]
+    return(rgb)
